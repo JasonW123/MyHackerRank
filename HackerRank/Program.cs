fd5171f0ac5020e310746c34076c1fa7f438112e
JasonW123 <jianxiong.wu@live.com>
2015-08-17 10:27:58 -0400

    Setup Project
    Setup Project.

---
+++ HackerRank/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+            var data = Console.ReadLine();
+            var dataNum = data.Split(' ');
+
+            var array = new List<long>();
+            for (var i = 0; i < t; i++)
+            {
+                var num = Convert.ToInt64(dataNum[i]);
+                array.Add(num);
+            }
+            Console.WriteLine(array.Sum());
+        }
+
+        private void LargeSum()
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+            var data = Console.ReadLine();
+            var dataNum = data.Split(' ');
+
+            var array = new List<long>();
+            for (var i = 0; i < t; i++)
+            {
+                var num = Convert.ToInt64(dataNum[i]);
+                array.Add(num);
+            }
+            Console.WriteLine(array.Sum());
+        }
+
+        private void SmallSum()
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+            var data = Console.ReadLine();
+            var dataNum = data.Split(' ');
+
+            var array = new List<int>();
+            for (var i = 0; i < t; i++)
+            {
+                var num = Convert.ToInt32(dataNum[i]);
+                array.Add(num);
+            }
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
